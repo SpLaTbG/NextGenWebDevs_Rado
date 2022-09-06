5a52938c02ce43a9770f046cc40530e6c88d74fb
Radoslav Ivanov <rado_rg2001@abv.bg>
2022-09-06 10:28:24 +0300

    html

---
+++ Buton.js
@@ -0,0 +1,17 @@
+console.log("banan")
+var content = document.getElementById("ime")
+var button = document.getElementById("show-more")
+
+button.onclick = function ( ){
+    
+    if(content.className == "open"){ 
+        content.className = "";
+        button.innerHTML = "Click me daddy"
+    } else{
+        content.className = "open"
+        button.innerHTML = "Reset me daddy "
+    
+
+
+    }
+ }
